8c70a4e22833eb5eb2ba951a69365d8ddd5e155f
Charlee Gordon <cgordon5025@gmail.com>
2022-10-19 11:48:53 -0400

    working on switching to promises

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const mysqlPromise = require('mysql2/promise')
 const cTable = require('console.table');
 
 require('dotenv').config()
@@ -76,7 +77,7 @@ const addEmp = [
   {
     type: 'list',
     message: "What is the Employee's role?",
-    choices: empArray,//needs to be an updated list including the new role
+    choices: roleArray,//needs to be an updated list including the new role
     name: 'addEmpRole'
   },
   {
@@ -88,7 +89,7 @@ const addEmp = [
   {
     type: 'list',
     message: "Who is the employee's manager?",
-    choices: empArray,//update list of possible managers? must link to the manager id
+    choices: managerArray,//update list of possible managers? must link to the manager id
     name: 'addEmpManager'
   }
 ]
@@ -108,31 +109,46 @@ const updateEmp = [
   }
 ]
 //need to call all the data down first since it will not be available until I view the info, due to the way I wrote this
-async function callEmps() {
-  var data = db.query("SELECT employees.id,employees.first_name,employees.last_name, roles.role_title, departments.dept_name, roles.salary, CONCAT (ManagerName.first_name,' ', ManagerName.last_name) AS Manager FROM employees LEFT JOIN employees AS ManagerName ON employees.manager_id = ManagerName.id JOIN roles ON employees.role_id = roles.id JOIN departments on employees.dept_id = departments.id ORDER BY employees.id;", function (err, results) {
-    results.forEach(emp => empArray.push(emp))
 
-  }
-  )
-  return data
+async function test() {
+  db.query('SELECT * FROM roles', function (err, results) {
+    //can call here but the issue is that I need to call without displaying and then possibly display once I select the thing
+    console.table(results)
+    // return results
+  })
+  //.then(results => console.table(results))
 }
+// async function callEmps() {
+//   db.query("SELECT employees.id,employees.first_name,employees.last_name, roles.role_title, departments.dept_name, roles.salary, CONCAT (ManagerName.first_name,' ', ManagerName.last_name) AS Manager FROM employees LEFT JOIN employees AS ManagerName ON employees.manager_id = ManagerName.id JOIN roles ON employees.role_id = roles.id JOIN departments on employees.dept_id = departments.id ORDER BY employees.id;", function (err, results) {
+//     return results
+//     // console.table(results)
+//     // results.forEach(emp => empArray.push(emp))
+//     //have this run, figure out a connecting point to get this data from the callFunction to the inquirer to display
+//   }
+//   ).then(results => console.table(results))
+// }
+
 async function init() {
   var mainMenu = await inquirer.prompt(mainMenuQuest)
-  callEmps()
+  await test()
   if (mainMenu.mainMenu == 'View All Wizards') {
-    callEmps().then(console.table(data))
+    //if this method needs to be promise
+    await test()
+    // console.table(empArray)
     // db.query("SELECT employees.id,employees.first_name,employees.last_name, roles.role_title, departments.dept_name, roles.salary, CONCAT (ManagerName.first_name,' ', ManagerName.last_name) AS Manager FROM employees LEFT JOIN employees AS ManagerName ON employees.manager_id = ManagerName.id JOIN roles ON employees.role_id = roles.id JOIN departments on employees.dept_id = departments.id ORDER BY employees.id;", function (err, results) {
     //   results.forEach(emp => empArray.push(emp))
     //   console.table(empArray)
-    await init()
+    init()
+    // })
   } else if (mainMenu.mainMenu == "Add New Wizard") {
     var newEmp = await inquirer.prompt(addEmp)
       .then(newEmpInput => {
-        db.query('INSERT INTO employees (first_name, last_name, role_id,dept_id,manager_id) VALUES (?,?,?,?,?)', newEmp.addFirstName, newEmp.addLastName, newEmp.addEmpRole, newEmp.addEmpDept, newEmp.addEmpManager)
+        //this is the correct syntax just need to put it in the new functions I create
+        db.query('INSERT INTO employees (first_name, last_name, role_id,dept_id,manager_id) VALUES (?,?,?,?,?)', newEmpInput.addFirstName, newEmpInput.addLastName, newEmpInput.addEmpRole, newEmpInput.addEmpDept, newEmpInput.addEmpManager)
       })
 
   } else if (mainMenu.mainMenu == "Update Wizard Role") {
-
+//somehting along the lines of based off name, selecting the individual by the name, and translating that to their unique ID
   } else if (mainMenu.mainMenu == "View All Roles") {
     ///due to the nature of the data, the employees is an intermediary between roles and dept, so the roles will have no dept_id
     db.query('SELECT * FROM roles', function (err, results) {
