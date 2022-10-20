6866208bf6f652022ecb5b701a05376ff17f5450
Charlee Gordon <cgordon5025@gmail.com>
2022-10-19 22:15:04 -0400

    code is working better now

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //setting up some arrays
-var empArray = [];
-var roleArray = []
-var deptArray = [];
+// getEmps()
+// getDepts()
 var managerArray = ["Albus Dumbledore", "Minerva McGonagall", "Severus Snape", "Filius Flitwick", "Pomona Sprout"];
 
 const db = mysql.createConnection(
@@ -26,7 +25,119 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employee_db database.`)
 );
+//lets call down the data to use when selecting the info
 
+
+
+
+const updateEmp = [
+  {
+    type: 'list',
+    message: "Which employee's role do you want to update?",
+    choices: ['people'],//list all the employees we have,mus thave the added people
+    name: 'updateEmpWho'
+  },
+  {
+    type: 'list',
+    message: "Which role do you want to assign the selected employee?",
+    choices: ['list of roles'],
+    name: 'updateEmpRole'
+  }
+]
+//establishing this so I can call it in the inquirer questions
+const roles = [];
+const depts = [];
+const emps = [];
+async function init() {
+  let roleArray = [];
+  let deptArray = [];
+  let empArray = []
+  await db.promise().query('SELECT * FROM roles').then(results =>
+    results[0].forEach(role => {
+      // console.log("myrole" + JSON.stringify(role.role_title)),
+      roles.push(role.role_title),
+        roleArray.push(role)
+    }
+    ))
+  await db.promise().query('SELECT * FROM departments').then(results =>
+    results[0].forEach(dept => {
+      // console.log("myrole" + JSON.stringify(role.role_title)),
+      depts.push(dept.dept_name),
+        deptArray.push(dept)
+    }
+    ))
+  await db.promise().query('SELECT * FROM employees').then(results =>
+    results[0].forEach(emp => {
+      // console.log("myrole" + JSON.stringify(role.role_title)),
+      emps.push(`${emp.first_name} ${emp.last_name}`),
+        empArray.push(emp)
+    }
+    ))
+
+  var mainMenu = await inquirer.prompt(mainMenuQuest)
+  if (mainMenu.mainMenu == 'View All Wizards') {
+    //if this method needs to be promise
+    await callEmps().then(([rows, fields]) =>
+      console.table(rows)
+    )
+    init()
+  } else if (mainMenu.mainMenu == "Add New Wizard") {
+    addEmp()
+  } else if (mainMenu.mainMenu == "Update Wizard Role") {
+    //somehting along the lines of based off name, selecting the individual by the name, and translating that to their unique ID
+  } else if (mainMenu.mainMenu == "View All Roles") {
+    ///due to the nature of the data, the employees is an intermediary between roles and dept, so the roles will have no dept_id
+    await callRoles().then(([rows, fields]) =>
+      console.table(rows)
+    )
+    init()
+  } else if (mainMenu.mainMenu == "Add Role") {
+    addRole()
+  } else if (mainMenu.mainMenu == "View All Departments") {
+    await callDepts().then(([rows, fields]) =>
+      console.table(rows)
+    )
+    init()
+
+  } else if (mainMenu.mainMenu == "Add Department") {
+    addDept()
+  } else if (mainMenu.mainMenu == "Quit") {
+    return "Goodbye!"
+  }
+}
+init()
+app.use((req, res) => {
+  res.status(404).end();
+});
+//display and run the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+//all functions
+async function callEmps() {
+  return db.promise().query("SELECT employees.id,employees.first_name,employees.last_name, roles.role_title, departments.dept_name, roles.salary, CONCAT (ManagerName.first_name,' ', ManagerName.last_name) AS Manager FROM employees LEFT JOIN employees AS ManagerName ON employees.manager_id = ManagerName.id JOIN roles ON employees.role_id = roles.id JOIN departments on employees.dept_id = departments.id ORDER BY employees.id;")
+}
+async function callRoles() {
+  return db.promise().query('SELECT * FROM roles')
+}
+async function callDepts() {
+  return db.promise().query('SELECT * FROM departments')
+}
+
+async function getRoles() {
+  let roleArray = [];
+  await db.promise().query('SELECT * FROM roles').then(results =>
+    results[0].forEach(role =>
+      // console.log("myrole" + JSON.stringify(role.role_title)),
+      roles.push(role.role_title),
+      roleArray.push(role.role_title)),
+    //  console.log(results[0]))
+  );
+  return roleArray
+  // (res => console.log(roleArray))
+}
 const mainMenuQuest = [
   {
     type: 'list',
@@ -43,200 +154,62 @@ const addDept = async () => {
       name: 'addDept'
     }
   )
-  let enteredDept = await db.promise().query('INSERT INTO departments(dept_name) VALUES (?)', [deptInput.addDept] )
+  let enteredDept = await db.promise().query('INSERT INTO departments(dept_name) VALUES (?)', [deptInput.addDept])
+  init()
 }
 
-addDept()
-const addDepartment = [
-  {
-    type: 'input',
-    message: 'What is the name of the department?',
-    name: 'addDept'
-  }
-]
-
-const addRole = [
-  {
-    type: 'input',
-    message: 'What is the name of the role',
-    name: 'addRoleName'
-  },
-  {
-    type: 'input',
-    message: 'What is the salary of the role?',
-    name: 'addRoleSalary'
-  }
-]
-
-const addEmp = [
-  {
-    type: 'input',
-    message: "What is the employee's first name?",
-    name: 'addFirstName'
-  },
-  {
-    type: 'input',
-    message: "What is the employee's last name?",
-    name: 'addLastName'
-  },
-  {
-    type: 'list',
-    message: "What is the Employee's role?",
-    choices: () => {
-      db.promise().query('SELECT * FROM roles').then(results =>
-        results[0].forEach(role =>
-          // console.log("myrole" + JSON.stringify(role.role_title)),
-          roleArray.push(role.role_title)),
-      );
-      return roleArray
+const addRole = async () => {
+  var roleInput = await inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the name of the role',
+      name: 'addRoleName'
     },
-    // getRoles(),//needs to be an updated list including the new role
-    name: 'addEmpRole'
-  },
-  {
-    type: 'list',
-    message: "Which department does this employee belong to?",
-    choices: "e",
-    name: 'addEmpDept'
-  },
-  {
-    type: 'list',
-    message: "Who is the employee's manager?",
-    choices: managerArray,//update list of possible managers? must link to the manager id
-    name: 'addEmpManager'
-  }
-]
-
-const updateEmp = [
-  {
-    type: 'list',
-    message: "Which employee's role do you want to update?",
-    choices: ['people'],//list all the employees we have,mus thave the added people
-    name: 'updateEmpWho'
-  },
-  {
-    type: 'list',
-    message: "Which role do you want to assign the selected employee?",
-    choices: ['list of roles'],
-    name: 'updateEmpRole'
-  }
-]
-//need to call all the data down first since it will not be available until I view the info, due to the way I wrote this
-//have int, to grab things from db and then a second function to do the asking
-async function callData() {
-  getRoles()
-  getEmps()
-  getDepts()
-}
-async function init() {
-  var mainMenu = await inquirer.prompt(mainMenuQuest)
-  if (mainMenu.mainMenu == 'View All Wizards') {
-    //if this method needs to be promise
-    await callEmps().then(([rows, fields]) =>
-      console.table(rows)
-    )
-    init()
-  } else if (mainMenu.mainMenu == "Add New Wizard") {
-    await inquirer.prompt(addEmp)
-      .then(newEmpInput => {
-        let query = `INSERT INTO employees 
-        (name, address) VALUES ?;`
-        let values =
-          [newEmpInput.addFirstName, newEmpInput.addLastName, newEmpInput.addEmpRole, newEmpInput.addEmpDept, newEmpInput.addEmpManager]
-        db.query(query, [values], (err, rows) => {
-          if (err) throw err;
-          console.log("All Rows Inserted");
-        })
-      })
-
-    //this is the correct syntax just need to put it in the new functions I create
-    // db.query('INSERT INTO employees (first_name, last_name, role_id,dept_id,manager_id) VALUES (?,?,?,?,?)', newEmpInput.addFirstName, newEmpInput.addLastName, newEmpInput.addEmpRole, newEmpInput.addEmpDept, newEmpInput.addEmpManager)
-
-
-  } else if (mainMenu.mainMenu == "Update Wizard Role") {
-    //somehting along the lines of based off name, selecting the individual by the name, and translating that to their unique ID
-  } else if (mainMenu.mainMenu == "View All Roles") {
-    ///due to the nature of the data, the employees is an intermediary between roles and dept, so the roles will have no dept_id
-    await callRoles().then(([rows, fields]) =>
-      console.table(rows)
-    )
-    init()
-  } else if (mainMenu.mainMenu == "Add Role") {
-    await inquirer.prompt(addRole)
-      .then(newRoleInput => {
-        let query = `INSERT INTO employees (role_title, salary) VALUES ?;`
-        let values =
-          [newRoleInput.addRoleName, newRoleInput.addRoleSalary]
-        db.query(query, [values], (err, rows) => {
-          if (err) throw err;
-          console.log("All Rows Inserted");
-        })
-      })
-    // .then(newRoleInput => {
-    //   db.query('INSERT INTO roles (role_title, salary) VALUES (?,?)', newRoleInput.addRoleName, newRoleInput.addRoleSalary)
-    // })
-
-  } else if (mainMenu.mainMenu == "View All Departments") {
-    await callDepts().then(([rows, fields]) =>
-      console.table(rows)
-    )
-    init()
-
-  } else if (mainMenu.mainMenu == "Add Department") {
-    await inquirer.prompt(addDepartment)
-      .then(newDeptInput => {
-        db.query('INSERT INTO departments (dept_name)', newDeptInput.addDept)
-      })
-
-  } else if (mainMenu.mainMenu == "Quit") {
-    return "Goodbye!"
-  }
-}
-// init()
-app.use((req, res) => {
-  res.status(404).end();
-});
-//display and run the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-
-//all functions
-async function callEmps() {
-  return db.promise().query("SELECT employees.id,employees.first_name,employees.last_name, roles.role_title, departments.dept_name, roles.salary, CONCAT (ManagerName.first_name,' ', ManagerName.last_name) AS Manager FROM employees LEFT JOIN employees AS ManagerName ON employees.manager_id = ManagerName.id JOIN roles ON employees.role_id = roles.id JOIN departments on employees.dept_id = departments.id ORDER BY employees.id;")
-  // console.table(results)
-  // results.forEach(emp => empArray.push(emp))
-  //have this run, figure out a connecting point to get this data from the callFunction to the inquirer to display
-}
-async function callRoles() {
-  return db.promise().query('SELECT * FROM roles')
-}
-
-var roleArray = []
-async function getRoles() {
-  db.promise().query('SELECT * FROM roles').then(results =>
-    results[0].forEach(role =>
-      // console.log("myrole" + JSON.stringify(role.role_title)),
-      roleArray.push(role.role_title)),
-    //  console.log(results[0]))
-  );
-  return roleArray
-  // (res => console.log(roleArray))
-}
-// db.promise().query('SELECT * FROM roles').then(([rows, fields]) =>
-// console.table(rows.role_title))
-async function callDepts() {
-  let dept = await db.promise().query('SELECT * FROM departments')
-  dept.map((deptOpt) => {
-    console.log(deptOpt.dept_name)
-    return {
-      name: deptOpt.dept_name
+    {
+      type: 'input',
+      message: 'What is the salary of the role?',
+      name: 'addRoleSalary'
     }
-  })
-
+  ]
+  )
+  let enteredRole = await db.promise().query('INSERT INTO roles (role_title, salary) VALUES (?,?)', [roleInput.addRoleName, roleInput.addRoleSalary])
+  init()
 }
-// callDepts()
-async function getEmps() {
-  return db.promise().query('SELECT * FROM employees')
+
+const addEmp = async () => {
+  var empInput = await inquirer.prompt(
+    [
+      {
+        type: 'input',
+        message: "What is the employee's first name?",
+        name: 'addFirstName'
+      },
+      {
+        type: 'input',
+        message: "What is the employee's last name?",
+        name: 'addLastName'
+      },
+      {
+        type: 'list',
+        message: "What is the Employee's role?",
+        choices: roles,
+        name: 'addEmpRole'
+      },
+      {
+        type: 'list',
+        message: "Which department does this employee belong to?",
+        choices: depts,
+        name: 'addEmpDept'
+      },
+      {
+        type: 'list',
+        message: "Who is the employee's manager?",
+        choices: emps,
+        name: 'addEmpManager'
+      }
+    ]
+  )
+
+  let enteredEmp = await db.promise().query('INSERT INTO employees (first_name, last_name, role_id,dept_id,manager_id) VALUES (?,?,?,?,?)', [empInput.addFireName, empInput.addLastName, empInput.addEmpRole, empInput.addEmpDept, empInput.addEmpManager])
+  init()
 }
